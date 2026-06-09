2d6c8cd0d4d759d1bd2d2a763b58fbbd504388fb
Thomas Isberg <thomas@tintonic.com>
2026-06-09 21:29:19 +0200

    PHP 8.5

--- lib/init_options.sh
+++ lib/init_options.sh
@@ -25,6 +25,8 @@ ONLY_PHP_8_0=false
 ONLY_PHP_8_1=false
 ONLY_PHP_8_2=false
 ONLY_PHP_8_3=false
+ONLY_PHP_8_4=false
+ONLY_PHP_8_5=false
 ONLY_XCODE=false
 PHP_5_6=true
 PHP_7_0=true
@@ -36,6 +38,8 @@ PHP_8_0=true
 PHP_8_1=true
 PHP_8_2=true
 PHP_8_3=true
+PHP_8_4=true
+PHP_8_5=true
 PHP_ENABLE=true
 PRESET_COMMON=false
 PRESET_MINIMAL=false
@@ -64,6 +68,7 @@ do
     --no-php-8-2)         PHP_8_2=false ;;
     --no-php-8-3)         PHP_8_3=false ;;
     --no-php-8-4)         PHP_8_4=false ;;
+    --no-php-8-5)         PHP_8_5=false ;;
     --no-php-enable)      PHP_ENABLE=false ;;
     --no-xcode-select)    INSTALL_XCODE=false ;;
     --only-apache)        ONLY_APACHE=true ;;
@@ -81,6 +86,7 @@ do
     --only-php-8-2)       ONLY_PHP_8_2=true ;;
     --only-php-8-3)       ONLY_PHP_8_3=true ;;
     --only-php-8-4)       ONLY_PHP_8_4=true ;;
+    --only-php-8-5)       ONLY_PHP_8_5=true ;;
     --only-xcode-select)  ONLY_XCODE=true ;;
     --p-common)           PRESET_COMMON=true ;;
     --p-minimal)          PRESET_MINIMAL=true ;;
@@ -97,11 +103,11 @@ done
 # ----------------------------------------------------------
 
 if $PRESET_COMMON; then
-    ONLY_PHP_8_3=true
+    ONLY_PHP_8_4=true
     INSTALL_XCODE=false
 elif $PRESET_MINIMAL; then
     INSTALL_MYSQL=false
-    ONLY_PHP_8_3=true
+    ONLY_PHP_8_4=true
     INSTALL_XCODE=false
 elif $ONLY_APACHE; then
     INSTALL_DNSMASQ=false
